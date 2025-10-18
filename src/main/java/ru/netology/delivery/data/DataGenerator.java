069205b9bb33262f5a71b3ee06995b03c2fdcff2
Aliya Voronkina <atleugabilova@mail.ru>
2025-10-18 16:34:30 +0500

    fix

---
+++ src/main/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,47 @@
+package ru.netology.delivery.data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class DataGenerator {
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        String[] cities = {"Москва", "Санкт-Петербург", "Казань", "Екатеринбург", "Новосибирск"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName() {
+        String[] lastNames = {"Иванов", "Петров", "Сидоров", "Кузнецов", "Попов"};
+        String[] firstNames = {"Иван", "Петр", "Алексей", "Сергей", "Дмитрий"};
+        return lastNames[new Random().nextInt(lastNames.length)] + " " + firstNames[new Random().nextInt(firstNames.length)];
+    }
+
+    public static String generatePhone() {
+        return "+7916" + String.format("%07d", new Random().nextInt(10000000));
+    }
+
+    public static UserInfo generateUser() {
+        return new UserInfo(generateCity(), generateName(), generatePhone());
+    }
+
+    public static class UserInfo {
+        private String city;
+        private String name;
+        private String phone;
+
+        public UserInfo(String city, String name, String phone) {
+            this.city = city;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getCity() { return city; }
+        public String getName() { return name; }
+        public String getPhone() { return phone; }
+    }
+}
--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,59 +1,34 @@
-package ru.netology.delivery.test;
-
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Selenide.$;
-
-class DeliveryTest {
+public class DeliveryTest {
 
     @BeforeEach
     void setup() {
-        Selenide.open("http://localhost:9999");
+        com.codeborne.selenide.Selenide.open("http://localhost:9999");
     }
 
     @Test
-    @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-        var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 4;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        DataGenerator.UserInfo user = DataGenerator.generateUser();
+        String firstDate = DataGenerator.generateDate(4);
+        String secondDate = DataGenerator.generateDate(7);
 
-        // Первое планирование
-        $("[data-test-id=city] input").setValue(validUser.getCity());
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(firstMeetingDate);
-        $("[data-test-id=name] input").setValue(validUser.getName());
-        $("[data-test-id=phone] input").setValue(validUser.getPhone());
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
+        // Заполняем форму
+        com.codeborne.selenide.Selenide.$("[data-test-id=city] input").setValue(user.getCity());
+        com.codeborne.selenide.Selenide.$("[data-test-id=date] input")
+                .sendKeys(org.openqa.selenium.Keys.chord(org.openqa.selenium.Keys.SHIFT,
+                                org.openqa.selenium.Keys.HOME),
+                        org.openqa.selenium.Keys.BACK_SPACE);
+        com.codeborne.selenide.Selenide.$("[data-test-id=date] input").setValue(firstDate);
+        com.codeborne.selenide.Selenide.$("[data-test-id=name] input").setValue(user.getName());
+        com.codeborne.selenide.Selenide.$("[data-test-id=phone] input").setValue(user.getPhone());
+        com.codeborne.selenide.Selenide.$("[data-test-id=agreement]").click();
+        com.codeborne.selenide.Selenide.$(".button").click();
 
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate),
-                        Duration.ofSeconds(15));
-
-        // Перепланирование
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(secondMeetingDate);
-        $(".button").click();
-
-        $("[data-test-id=replan-notification] .notification__content")
-                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"),
-                        Duration.ofSeconds(15));
-
-        $("[data-test-id=replan-notification] .button").click();
-
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate),
-                        Duration.ofSeconds(15));
+        // Проверяем
+        com.codeborne.selenide.Selenide.$("[data-test-id=success-notification] .notification__content")
+                .shouldHave(com.codeborne.selenide.Condition.text("Встреча успешно запланирована на " + firstDate),
+                        java.time.Duration.ofSeconds(15));
     }
 }
